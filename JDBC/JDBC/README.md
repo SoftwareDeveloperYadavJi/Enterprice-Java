ea83fc7c56aa235c5693aec64275aa4f1ccfcca2
Nitin Yadav <108917474+SoftwareDeveloperYadavJi@users.noreply.github.com>
2024-07-08 18:38:10 +0530

    JDBC

---
+++ JDBC/JDBC/README.md
@@ -0,0 +1,30 @@
+## Folder Structure
+
+The workspace contains two folders by default, where:
+
+- `src`: the folder to maintain sources
+- `lib`: the folder to maintain dependencies
+
+Meanwhile, the compiled output files will be generated in the `bin` folder by default.
+
+> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
+
+
+
+# Connecting to MySQL Database using JDBC in Java
+
+- This guide explains how to connect to a MySQL database using JDBC in Java. Follow these steps to set up the connection and execute SQL queries.
+
+# Prerequisites
+
+- `Java Development Kit (JDK)`: Make sure you have JDK installed on your system. You can download it from Oracle's official site.
+ 
+- `MySQL Server`: Install MySQL Server on your machine. You can download it from MySQL's official site.
+
+- `MySQL JDBC Driver`: Download the MySQL Connector/J from here.
+
+
+
+
+
+
---
+++ JDBC/JDBC/src/App.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+class App {
+    public static void main(String[] args) throws Exception {
+        String url = "jdbc:mysql://localhost:3306/student";
+        String username = "root";
+        String password = "root";
+        String query = "INSERT INTO students (name, age, major) VALUES ('Nitin', 21, 'Computer Science')";
+
+        // Load and register the JDBC driver
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        // Establish the connection
+        Connection con = DriverManager.getConnection(url, username, password);
+        System.out.println("Connection Established successfully");
+
+        // Create a statement
+        Statement st = con.createStatement();
+
+        // Execute the INSERT query
+        int rowsAffected = st.executeUpdate(query);
+        System.out.println("Rows affected: " + rowsAffected);
+
+        // Close the statement and connection
+        st.close();
+        con.close();
+        System.out.println("Connection Closed....");
+    }
+}
